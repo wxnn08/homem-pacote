867618b0f914c580c251578865aa26a86e887b54
wxnn08 <wesleypersilva@gmail.com>
2018-07-17 22:16:21 -0300

    movendo pastas

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# homem-pacote
-Pacman game
+# Homem Pacote
+
+Um jogo do grande clássico Pacman (ou em português, homem pacote (ou em inglês, package man (ou em português, homem da idade de embalagem))).
---
+++ pacman/Principal.java
@@ -0,0 +1,11 @@
+import jogo.*;
+
+public class Principal {
+	public static void main(String[] args) {
+		Labirinto l = new Labirinto(20);
+		
+		GameManager gm = new GameManager((l));
+		gm.play();
+	
+	}
+}
