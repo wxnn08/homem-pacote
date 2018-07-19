e0cb70c76e2d157e8d1099dfe36f0f204cd3204e
Andre <jomonzito2010@gmail.com>
2018-07-19 14:56:52 -0300

    Atualizados Labirinto e Principal, agora o labirinto aceita qualquer mapa desenhando no mapa.txt

--- pacman/Principal.java
+++ pacman/Principal.java
@@ -1,8 +1,8 @@
 import jogo.*;
 
 public class Principal {
-	public static void main(String[] args) {
-		Labirinto l = new Labirinto(20);
+	public static void main(String[] args) throws Exception {
+		Labirinto l = new Labirinto(15, 15);
 		
 		GameManager gm = new GameManager((l));
 		gm.play();
--- pacman/jogo/Labirinto.java
+++ pacman/jogo/Labirinto.java
@@ -1,41 +1,64 @@
 package jogo;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Labirinto {
 
 	char[][] labirinto;
-	private int dimensao; //Todos os labirintos serão matrizes quadradas, de acordo com essa linha. Pode mudar, obviamente
-	
-	public Labirinto(int dimensao) {
-	
-		this.setDimensao(dimensao);
-		labirinto = new char[this.getDimensao()][this.getDimensao()];
+	private int altura, largura;
 
-		//TO DO: Montar o labirinto a partir de um arquivo
+	public Labirinto(int a, int l) throws Exception {
+	
+		this.setAltura(a);
+		this.setLargura(l);
+		labirinto = new char[this.getAltura()][this.getLargura()];
+
+		BufferedReader arquivo = new BufferedReader(new FileReader("mapa.txt"));
 		
-		for(int i = 0; i<this.getDimensao(); i++) {
-			for (int j = 0; j<this.getDimensao(); j++) {
-				labirinto[i][j] = '#';
+		for(int i = 0; i<this.getAltura(); i++) {
+			for (int j = 0; j<this.getLargura(); j++) {
+				labirinto[i][j] = devolveChar((char)arquivo.read());
 			}
-		}		
+			arquivo.read(); //Jogando fora o caractere '/n'
+		}
+		arquivo.close();	
 	}
 	
-	public void setDimensao(int n) {
-		this.dimensao = n;
+	private char devolveChar(char n) {
+		if (n=='#')
+			return '#';
+		else if (n=='C')
+			return 'C';
+		else if (n=='G')
+			return 'G';
+		else
+			return '.';
+	}
+
+	public void setAltura(int n) {
+		this.altura = n;
 	}
 	
-	public int getDimensao() {
-		return this.dimensao;
+	public int getAltura() {
+		return this.altura;
+	}
+
+	public void setLargura(int n) {
+		this.largura = n;
+	}
+	
+	public int getLargura() {
+		return this.largura;
 	}
 	
 	public void mostrarLabirinto() {
 		
-		for(int i = 0; i<this.getDimensao(); i++) {
-			for (int j = 0; j<this.getDimensao(); j++) {
+		for(int i = 0; i<this.getAltura(); i++) {
+			for (int j = 0; j<this.getLargura(); j++) {
 				System.out.print(this.labirinto[i][j]);
 			}
 			
 			System.out.println();
 		}
-	}
-	
+	}	
 }
