7a9c590bdb6c55584fd533899ba5ce971f09d047
Bruno Masunaga <brunomasunaga@localhost.localdomain>
2018-07-26 00:04:13 -0300

    Implementada a funcionalidade de se obter vários fantasmas utilizando vetor

--- pacman/jogo/Fantasma.java
+++ pacman/jogo/Fantasma.java
@@ -14,29 +14,46 @@ public class Fantasma extends Personagem {
 		ultimaEscolha = rand.nextInt(4);
 	}
 
+	int[] getPositions(){
+		int[] positions = new int[2];
+		positions[0] = posy;
+		positions[1] = posx;
+		return positions;
+	}
+
 	void setUltimoQuadrado (char c){
 		ultimoQuadrado = c;
 	}
 
+	char getUltimoQuadrado (){
+		return ultimoQuadrado;
+	}
+
+	//função que verifica se este fantasma pegou o pacman
 	void verificaMatouPacman(){
-		int y = Pacman.getPositions()[0];
-		int x = Pacman.getPositions()[1];
-		if (posy == y && posx == x)
-			GameManager.mortePacman(posy, posx);
+		int y = Pacman.getPositions()[0]; //captura posicao y do pacman
+		int x = Pacman.getPositions()[1]; //captura posicao x do pacman
+		if (posy == y && posx == x) //compara com as posições do fantasma
+			GameManager.mortePacman(posy, posx); //chama função de morte eles ocupam a mesma posição
 	}
 
 	private int sorteiaNumero() {
 		boolean[] posPossivel = new boolean[4]; // [Esq, Cim, Dir, Baixo]
 		int numPosPossiveis = 0;
-		if (getProxQuad(1, ultimaEscolha, posy, posx) != '\u2588')
+		if (getProxQuad(1, ultimaEscolha, posy, posx) != '\u2588' &&
+		getProxQuad(1, ultimaEscolha, posy, posx) != '\u15E3') //se há outro fantasma na frente, então ele não avança
 			posPossivel[ultimaEscolha] = true;
 		if (ultimaEscolha % 2 == 0){
-			if (getProxQuad(1, 1, posy, posx) != '\u2588') posPossivel[1] = true;
-			if (getProxQuad(1, 3, posy, posx) != '\u2588') posPossivel[3] = true;
+			if (getProxQuad(1, 1, posy, posx) != '\u2588' &&
+			getProxQuad(1, 1, posy, posx) != '\u15E3') posPossivel[1] = true; //considera outro fantasma como obstáculo
+			if (getProxQuad(1, 3, posy, posx) != '\u2588' &&
+			getProxQuad(1, 3, posy, posx) != '\u15E3') posPossivel[3] = true; //considera outro fantasma como obstáculo
 		}
 		else {
-			if (getProxQuad(1, 0, posy, posx) != '\u2588') posPossivel[0] = true;
-			if (getProxQuad(1, 2, posy, posx) != '\u2588') posPossivel[2] = true;
+			if (getProxQuad(1, 0, posy, posx) != '\u2588' &&
+			getProxQuad(1, 0, posy, posx) != '\u15E3') posPossivel[0] = true; //considera outro fantasma como obstáculo
+			if (getProxQuad(1, 2, posy, posx) != '\u2588' &&
+			getProxQuad(1, 2, posy, posx) != '\u15E3') posPossivel[2] = true; //considera outro fantasma como obstáculo
 		}
 		for (int i = 0; i < 4; i++)
 			if (posPossivel[i] == true) numPosPossiveis++;
@@ -55,9 +72,10 @@ public class Fantasma extends Personagem {
 
 	@Override
 	void move(){
-		verificaMatouPacman();
+		verificaMatouPacman(); //verifica se pegou o pacman antes de se mover
 		ultimaEscolha = sorteiaNumero();
 		char quadGuardado = getProxQuad(1, ultimaEscolha, posy, posx);
+		if (quadGuardado == '\u15E3') quadGuardado = '\u2022'; //se por acaso cruzar com outro fantasma, entao ele printa um ponto
 		lab.mudarQuadrado(ultimoQuadrado, posx, posy);
 		switch (ultimaEscolha) {
 			case 0: // Esquerda
@@ -74,6 +92,6 @@ public class Fantasma extends Personagem {
 				posy++; break;
 		}
 		ultimoQuadrado = quadGuardado;
-		verificaMatouPacman();
+		verificaMatouPacman(); //verifica se pegou o pacman depois de se mover
 	}
 }
--- pacman/jogo/GameManager.java
+++ pacman/jogo/GameManager.java
@@ -4,10 +4,14 @@ import java.util.concurrent.TimeUnit;
 public class GameManager{
 
 	static Labirinto lab;
+	//declaracao estática dos personagens: precisa fazer funcao que recebe o num
+	//de fantasmas como paramtro
 	static Pacman pacman;
-	static Fantasma fantasma;
+	static Fantasma[] fantasmas = new Fantasma[10];
+
     private static int pontos = 0;
     private static int vidas = 3;
+	private static int fantasmasSpawn = 0;
 
     public GameManager() throws Exception{
         lab = new Labirinto();
@@ -41,40 +45,63 @@ public class GameManager{
 	 	return false;
 	}
 
+	//funcao de morte do pacman
 	static void mortePacman(int posy, int posx){
-		setVidas(getVidas()-1);
-		pacman.spawn();
-		fantasma.spawn();
-		fantasma.setUltimoQuadrado(' ');
-		lab.mudarQuadrado(' ', posx, posy);
-		Controle.setActivedKey(0);
+		setVidas(getVidas()-1); //atualiza vidas
+		pacman.spawn(); //reseta posição do pacaman
+		for (int i = 0; i < fantasmas.length; i++){
+			int fposy = fantasmas[i].getPositions()[0]; //captura posicao y do fantasma i
+			int fposx = fantasmas[i].getPositions()[1]; //captura posicao x do fantasma i
+			lab.mudarQuadrado(fantasmas[i].getUltimoQuadrado(), fposx, fposy); //faz com que todos os fantasmas sumam
+		    fantasmas[i] = null; //anula o vetor de fantasmas para recomeçar o jogo
+			fantasmasSpawn = 0; //anula o contador de fantasmas spawnados
+		}
+		lab.mudarQuadrado(' ', posx, posy); //apaga o quadrado onde houve morte do pacman
+		Controle.setActivedKey(0); //anula a tecla do controlador para pausar o fluxo de jogo
 	}
 
     public void play(){
         try{
-			Controle c = new Controle();
-			pacman = new Pacman(lab.getSpawnPacman(0), lab.getSpawnPacman(1));
-			fantasma = new Fantasma(lab.getSpawnFantasma(0), lab.getSpawnFantasma(1));
-			pacman.spawn();
-			fantasma.spawn();
-	        while(vidas != 0){
-				if (start() == true){
-					fantasma.move();
-				    pacman.move();
+			Controle c = new Controle(); //faz a janela do swing aparecer
+			pacman = new Pacman(lab.getSpawnPacman(0), lab.getSpawnPacman(1)); //cria o pacman
+			pacman.spawn(); //spawna o pacman
+	        while(vidas != 0){ //loop que controla o game over
+				if (start() == true){ //determina o fluxo do jogo, se start() = false o jogo esta pausado
+					if (fantasmasSpawn != fantasmas.length){ //se ainda existem fantasmas nao spawnados
+						if (fantasmasSpawn == 0){ //se nenhum foi spawnado
+							fantasmas[0] = new Fantasma(lab.getSpawnFantasma(0), lab.getSpawnFantasma(1)); //instancia o 1st fantasma
+							fantasmas[0].spawn(); //spawna o primeiro fantasma
+						}
+						else{ //se já spawnou algum fantasma, então
+						    if(lab.getQuadrado(lab.getSpawnFantasma(1),lab.getSpawnFantasma(0)) != '\u15E3'){ //se nao tem fantasma no spawn
+								fantasmas[fantasmasSpawn] = new Fantasma(lab.getSpawnFantasma(0), lab.getSpawnFantasma(1)); //instancia novo fantasma
+								fantasmas[fantasmasSpawn].spawn(); //spawna novo fantasma
+							}
+						}
+						fantasmasSpawn++; //atualiza numero de fantasmas spawnados
+					}
+					for (int i = 0; i < fantasmas.length; i++){ //para todos os fantasmas
+						if (fantasmas[i] != null) //se o fantasma em questão foi spawnado
+							fantasmas[i].move(); //então ele pode se mover
+					}
+				    pacman.move(); //o pacman se move
 			        clear();
 			        tela();
 			    }
-			    else {
+
+			    else { // o jogo está pausado
 			        clear();
 			        tela();
 			        System.out.println("Utilize as setas para começar!");
 			    }
-			    TimeUnit.MILLISECONDS.sleep(150);
+			    TimeUnit.MILLISECONDS.sleep(150); //taxa de atualização da tela (independe do fluxo do jogo)
 			}
-			if (vidas == 0){
+
+			if (vidas == 0){ //fora do loop que controla o game over, se as vidas são zeradas, então
 				System.out.println("Game over!");
-				c.setVisible(false);
+				c.setVisible(false); //fecha o swing
 			}
+
         } catch (Exception e){}
     }
 }
